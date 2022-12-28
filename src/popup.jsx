c342c648aa940831cef406748a722ec051d21571
idirmosh <contact@idirmosh.dev>
2022-12-28 15:36:30 -0500

    tailwindcss/webpack setup

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import "./tailwind.css";
 
 const HTML_ROOT_ID = "react-root";
 
 function Popup() {
   return (
-    <div>
-      <h1>hello from popup.jsx webpack updated</h1>
+    <div className="w-44">
+      <h1 className="text-red-400">hello from popup.jsx webpack updated</h1>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
   },
   plugins: [
